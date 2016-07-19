6f21e03445b569cf2f2d1cb8b7777310555917e3
German Rodrigo Romarion <grromarion90@gmail.com>
2016-07-18 22:03:09 -0300

    Attempt to make nodes selectable

--- src/main/resources/web/js/graph-visualization.js
+++ src/main/resources/web/js/graph-visualization.js
@@ -18,7 +18,7 @@ function drawGraph(classes) {
     
     var config = {
     	node: [{
-    		show: 'hovers',
+    		show: 'rightClickNode',
     		hide: 'hovers',
     		cssClass: 'sigma-tooltip',
     		position: 'left',
@@ -66,6 +66,7 @@ function drawGraph(classes) {
         });
         classesSize--;
         if(classesSize == 0) {
+        	sigma.renderers.def = sigma.renderers.canvas;
             var s = new sigma({
                 graph: g,
                 renderer: {
@@ -79,14 +80,63 @@ function drawGraph(classes) {
                     enableEdgeHovering: true,
                     edgeHoverColor: 'edge',
                     edgeHoverSizeRatio: 1,
+                    edgeHoverHighlightNodes: 'circle',
                     edgeHoverExtremities: true,
                     animationsTime: 1500,
                     dragNodeStickiness: 0.01,
                     nodeBorderSize: 2,
-                    defaultNodeBorderColor: '#000'
+                    defaultNodeBorderColor: '#000',
+                    defaultNodeActiveBorderColor: '#fff',
+                    defaultNodeActiveOuterBorderColor: 'rgb(236, 81, 72)',
+                    nodeHaloColor: 'rgba(236, 81, 72, 0.2)',
+                    nodeHaloSize: 50
                 }
             });
             sigma.plugins.tooltips(s, s.renderers[0], config);
+            var activeState = sigma.plugins.activeState(s);
+
+	        // Initialize the dragNodes plugin:
+	        var dragListener = sigma.plugins.dragNodes(s, s.renderers[0], activeState);
+	        var select = sigma.plugins.select(s, activeState);
+	        var keyboard = sigma.plugins.keyboard(s, s.renderers[0]);
+	        select.bindKeyboard(keyboard);
+	        var lasso = new sigma.plugins.lasso(s, s.renderers[0], {
+        	  'strokeStyle': 'rgb(236, 81, 72)',
+        	  'lineWidth': 2,
+        	  'fillWhileDrawing': true,
+        	  'fillStyle': 'rgba(236, 81, 72, 0.2)',
+        	  'cursor': 'crosshair'
+        	});
+
+        	select.bindLasso(lasso);
+        	lasso.activate();
+        	// halo on active nodes:
+        	function renderHalo() {
+        	  s.renderers[0].halo({
+        	    nodes: activeState.nodes()
+        	  });
+        	}
+
+        	s.renderers[0].bind('render', function(e) {
+        	  renderHalo();
+        	});
+        	keyboard.bind('32+83', function() {
+    		  if (lasso.isActive) {
+    		    lasso.deactivate();
+    		  } else {
+    		    lasso.activate();
+    		  }
+    		});
+        	// Listen for selectedNodes event
+        	lasso.bind('selectedNodes', function (event) {
+        	  setTimeout(function() {
+        	    lasso.deactivate();
+        	    s.refresh({ skipIdexation: true });
+        	  }, 0);
+        	});
+	        // Curve parallel edges:
+//	        sigma.canvas.edges.autoCurve(s);
+	        s.refresh();
         }
     });
 }
